00e29128fb17a4f37f282abe77ee5c010a351cc0
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-18 18:06:14 -0700

    testing v10

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  defaultCommandTimeout: 90000,
+  e2e: {
+    baseUrl: 'https://app.pitch.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+}
